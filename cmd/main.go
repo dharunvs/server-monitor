cf09c028e93acb25cf64f9a066ae592c189a68c4
Dharun Sivakumar <dharunsivakumar002@gmail.com>
2025-01-08 12:58:25 +0000

    Updates

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	// "database/sql"
-	// "sync"
+	"sync"
 	// "fmt"
 
 	_ "github.com/lib/pq" 
 	"root/config"
 	"root/logger"
 	"root/connection"
-	// "root/monitor"
-	// "root/notifier"
-	"root/backup"
+	"root/monitor"
+	"root/notifier"
+	// "root/backup"
 )
 
 func main() {
@@ -22,32 +22,32 @@ func main() {
 		return
 	}
 
-	// notificationChannel := make(chan string)
+	notificationChannel := make(chan string)
 
 	_, err = connection.GetDatabase(&cfg.Database)
-	if err != nil {
-		logger.Error(err, )
-	}
-
-	// var wg sync.WaitGroup
-
-	// wg.Add(3)
-	// go notifier.StartNotifier(notificationChannel, &wg)
-
-	// tableChannels := make(map[string]chan string)
-	// tableChannels["monitoringdata"] = make(chan string)
-
-	// connection.WriteToDB()
-	// go monitor.StartServerMonitoring(cfg, &wg)
-	// go monitor.StartSelfMonitoring(cfg, &wg)
-
-	// file, err := backup.DumpDatabase(&cfg.Backup.SourceDB, "monitoringmodule", "/home/dharunvs/Documents/serverMonitoring/bkps/")
-	err = backup.DumpAndRestore(&cfg.Backup.SourceDB, &cfg.Backup.DestinationDBs[0], "vs_test_database","/home/dharunvs/Documents/serverMonitoring/bkps/")
 	if err != nil {
 		logger.Error(err)
 	}
 
-	// wg.Wait()
+	var wg sync.WaitGroup
+
+	wg.Add(3)
+	go notifier.StartNotifier(notificationChannel, &wg)
+
+	tableChannels := make(map[string]chan string)
+	tableChannels["monitoringdata"] = make(chan string)
+
+	// connection.WriteToDB()
+	go monitor.StartServerMonitoring(cfg, &wg)
+	go monitor.StartSelfMonitoring(cfg, &wg)
+
+	// file, err := backup.DumpDatabase(&cfg.Backup.SourceDB, "monitoringmodule", "/home/dharunvs/Documents/serverMonitoring/bkps/")
+	// err = backup.DumpAndRestore(&cfg.Backup.SourceDB, &cfg.Backup.DestinationDBs[0], "vs_test_database","/home/dharunvs/Documents/serverMonitoring/bkps/")
+	// if err != nil {
+	// 	logger.Error(err)
+	// }
+
+	wg.Wait()
 
 	// close(tableChannels["monitoringdata"] )
 	// logger.Info("MonitoringModule ended", connString)
--- go.mod
+++ go.mod
@@ -1,29 +1,20 @@
 module root
 
-go 1.18
+go 1.21
+
+toolchain go1.23.4
 
 require (
-	github.com/bramvdbogaerde/go-scp v1.4.0 // indirect
-	github.com/go-ole/go-ole v1.2.6 // indirect
-	github.com/go-ping/ping v1.1.0 // indirect
+	github.com/bramvdbogaerde/go-scp v1.4.0
+	github.com/go-ping/ping v1.1.0
+	github.com/habx/pg-commands v0.6.1
+	github.com/lib/pq v1.10.9
+	golang.org/x/crypto v0.24.0
+)
+
+require (
 	github.com/google/uuid v1.2.0 // indirect
-	github.com/habx/pg-commands v0.6.1 // indirect
-	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
-	github.com/jackc/pgconn v1.14.3 // indirect
-	github.com/jackc/pgio v1.0.0 // indirect
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgtype v1.14.0 // indirect
-	github.com/jackc/pgx/v4 v4.18.3 // indirect
-	github.com/lib/pq v1.10.9 // indirect
-	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
-	github.com/tklauser/go-sysconf v0.3.14 // indirect
-	github.com/tklauser/numcpus v0.8.0 // indirect
-	github.com/yusufpapurcu/wmi v1.2.4 // indirect
-	golang.org/x/crypto v0.24.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
 )
